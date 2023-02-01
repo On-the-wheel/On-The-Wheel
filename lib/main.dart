142196f0ef14cc6cd89f28074e9f5afc8d5e715d
king0404 <xhakxh147@handong.ac.kr>
2023-02-01 13:56:44 +0900

    kjh first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:onthewheelpractice/start_main.dart';
 
 import 'Home.dart';
 
-void main() {
-  runApp(const MyApp());
+// void main() {
+//   runApp( MyApp());
+// }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // 데이터를 받아올때까지 실행시켜줄때까지 기다려주겠다
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // const MyApp(super.key});
 
   // This widget is the root of your application.
   @override
--- lib/map/newPlace.dart
+++ lib/map/newPlace.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
-import '../Home.dart';
 import 'naverMap.dart';
 import 'newlaceNext.dart';
 
